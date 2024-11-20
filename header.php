ac33e47808bf3703027cb9219436259f4d029cf1
slim abida <slimabida21@gmail.com>
2024-11-20 20:23:08 +0100

    stylisation navbar root.css

--- header.php
+++ header.php
@@ -5,11 +5,11 @@
                 <a href="#">Portfolio.</a>
             </div>
             <ul class="nav-links">
-                <li><a href="#home">Accueil</a></li>
-                <li><a href="#about">A Propos</a></li>
-                <li><a href="#portfolio">Portfolio</a></li>
-                <li><a href="#services">Services</a></li>
-                <li><a href="#contact">Contact</a></li>
+                <li><a href="#home" class="btn">Accueil</a></li>
+                <li><a href="#about" class="btn">A Propos</a></li>
+                <li><a href="#portfolio" class="btn">Portfolio</a></li>
+                <li><a href="#services" class="btn">Services</a></li>
+                <li><a href="#contact" class="btn">Contact</a></li>
             </ul>
         </div>
     </nav>
