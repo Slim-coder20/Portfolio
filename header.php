f0710466abe4cc7bf445031e197b929e91952638
slim abida <slimabida21@gmail.com>
2024-11-20 19:45:37 +0100

    Modifiocation header.php

--- header.php
+++ header.php
@@ -1,9 +1,16 @@
-<link rel="stylesheet" href="./style.css">
-
-<nav>
-    <ul>
-        <li><a href="index.php">Accueil</a></li>
-        <li><a href="projets.php">Projets</a></li>
-        <li><a href="contact.php">Contact</a></li>
-    </ul>
-</nav>
+<header>
+    <nav class="navbar">
+        <div class="container">
+            <div class="brand">
+                <a href="#">Portfolio.</a>
+            </div>
+            <ul class="nav-links">
+                <li><a href="#home">Accueil</a></li>
+                <li><a href="#about">A Propos</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
